d0ade5dc77945f43befc8595c78acbc1ab0cdc9e
ChatPion <chatpion@gmail.com>
2017-05-08 14:11:59 +0200

    Removed useless flip + fixed agent's rendering

--- UI.py
+++ UI.py
@@ -1,25 +1,29 @@
 import pygame
 from pygame.locals import *
+from shoot_game import Game
+
 pygame.init()
 game = Game(1/4, 5, [0, 0])
+
 fenetre = pygame.display.set_mode((64*(2*game.width + 1), 320))
-perso = pygame.image.load("perso.png").convert()
+perso = pygame.image.load("perso.png").convert_alpha()
 bullet = pygame.image.load("bullet.png").convert_alpha()
-fenetre.blit(perso, (320, 256))
-pygame.display.flip()
+
 continuer = 1
 while continuer:
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = 0
-            
-    pygame.display.flip()
+
+    perso_coords = (0, 0)
     if game.is_jumping == 0:
-        fenetre.blit(perso, (320, 256))
+        perso_coords = (320, 256)
     elif game.is_jumping == 1:
-        fenetre.blit(perso, (320, 192))
+        perso_coords = (320, 192)
     else:
-        fenetre.blit(perso, (320, 128))
+        perso_coords = (320, 128)
+    fenetre.blit(perso, perso_coords)
+
     for i in game.bullets:
         fenetre.blit(bullet, (64*(5+i[0]), 256))
     for i in game.deadbullets:
