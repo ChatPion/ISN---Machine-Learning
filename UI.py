26aa32aaf8ed7e4b25cbf5355ef22e6fd97d3a0b
rubikbeggar <sebonduelle@free.fr>
2017-05-08 14:03:37 +0200

    Second step : added bullets and fixed dimensions

--- UI.py
+++ UI.py
@@ -1,29 +1,28 @@
 import pygame
 from pygame.locals import *
 pygame.init()
-
-fenetre = pygame.display.set_mode((640, 480))
+game = Game(1/4, 5, [0, 0])
+fenetre = pygame.display.set_mode((64*(2*game.width + 1), 320))
 perso = pygame.image.load("perso.png").convert()
-fenetre.blit(perso, (300, 380))
+bullet = pygame.image.load("bullet.png").convert_alpha()
+fenetre.blit(perso, (320, 256))
 pygame.display.flip()
-is_jumping = 0
 continuer = 1
 while continuer:
-    if is_jumping == 0:
-        is_jumping = 2
-    else:
-        is_jumping -= 1
-        
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = 0
             
     pygame.display.flip()
-    if is_jumping == 0:
-        fenetre.blit(perso, (300, 380))
+    if game.is_jumping == 0:
+        fenetre.blit(perso, (320, 256))
     elif game.is_jumping == 1:
-        fenetre.blit(perso, (300, 280))
+        fenetre.blit(perso, (320, 192))
     else:
-        fenetre.blit(perso, (300, 180))
+        fenetre.blit(perso, (320, 128))
+    for i in game.bullets:
+        fenetre.blit(bullet, (64*(5+i[0]), 256))
+    for i in game.deadbullets:
+        fenetre.blit(bullet, (64*(5+i[0]), 256))
     pygame.display.flip()
     
