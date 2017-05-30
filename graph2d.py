4b48ef55aa9fa655f86155870af3d76679d8a5ba
ChatPion <chatpion@gmail.com>
2017-05-30 23:54:07 +0200

    Simplified

--- graph2d.py
+++ graph2d.py
@@ -27,7 +27,7 @@ def load_test(path, agent_list):
 
     for i in range(agents_nb):
         dic = {}
-        load_data = data["data"+str(i)]
+        load_data = data[i]
         for k, v in load_data.items():
             dic[literal_eval(k)] = v
         agent_list[i].actions_value = dic
@@ -54,7 +54,7 @@ def test(agents_nb, steps_nb = 10, probability = 0.33, test_duration = 50, train
     file = open ("saves/stat2d"+str(agents_nb)+".json", "w")
     data = {}
     for i in range(agents_nb):
-        data["data"+str(i)] = {str(k): v for k,v in agent_list[i].actions_value.items()}
+        data[i] = {str(k): v for k,v in agent_list[i].actions_value.items()}
     to_write = json.dumps({
         'options': {
             'training_params': training_params,
