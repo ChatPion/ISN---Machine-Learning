ecad1da66d0990f7ef3d61fba59b34ec1ab24b6d
rubikbeggar <sebonduelle@free.fr>
2017-06-04 16:25:44 +0200

    P

--- graph2d.py
+++ graph2d.py
@@ -31,13 +31,13 @@ def load_test(path, agent_list):
             dic[literal_eval(k)] = v
         agent_list[i].actions_value = dic
     
-    return training_params, probability, agents_nb, data['old_results'], agent_list
+    return test_duration, probability, agents_nb, data['old_results'], agent_list
 
-def test(agents_nb, continued = False, erase = True, steps_nb = 70, probability=0.33, test_duration = 200, alpha=0.3, gamma=0.8, epsilon=0.8, training_params={'cycle_nb': 5, 'prob_step': 10, 'game_duration': 20}):
+def test(agents_nb, continued = False, erase = True, steps_nb = 100, probability=0.33, test_duration = 400, alpha=0.3, gamma=0.8, epsilon=0.8, training_params={'cycle_nb': 1, 'prob_step': 10, 'game_duration': 20}):
     agent_list = [Agent() for i in range(agents_nb)]
     old_results = []
     if continued and agent_exists("stat2d"+str(agents_nb)):
-        training_params, probability, agents_nb, old_results, agent_list = load_test("stat2d"+str(agents_nb), agent_list)
+        test_duration, probability, agents_nb, old_results, agent_list = load_test("stat2d"+str(agents_nb), agent_list)
     hits = old_results + [[0 for i in range(agents_nb)] for j in range(steps_nb)]
     
     game = Game(probability, 5)
@@ -75,28 +75,28 @@ def test(agents_nb, continued = False, erase = True, steps_nb = 70, probability=
         file = open ("saves/stat2d"+str(agents_nb)+".json", "r")
     return hits
 
-values= test(10, continued = True, test_duration=200, steps_nb = 160, probability=0.66, alpha=0.1)
+values= test(10)
 x = len(values)
 y = len(values[0])
 
-def show_plot(array, rows, columns, all_agents=False, mean=True, intervals=True, std=False, x1=0, x2=5*x, y1=0, y2=0.6):
+def show_plot(array, rows, columns, all_agents=False, mean=True, intervals=False, std=False, x1=0, x2=x, y1=0, y2=0.6):
     
     if all_agents == True:
         for i in range(columns):
-            plt.plot([i*5 for i in range(rows)], [array[j][i] for j in range(rows)], "r-")
+            plt.plot([i for i in range(rows)], [array[j][i] for j in range(rows)], "r-")
             
     if mean == True:
-        plt.plot([i*5 for i in range(rows)], [np.mean(array[row]) for row in range(rows)], "g-")
+        plt.plot([i for i in range(rows)], [np.mean(array[row]) for row in range(rows)], "g-")
         
-    if intervals == True:
+    if intervals == True: # Displays the standard deviation around the mean curve
         for i in range(rows):
-            plt.plot([i*5, i*5], [np.mean(array[i]) - np.std(array[i]), np.mean(array[i]) + np.std(array[i])], "b-", linewidth = 0.5)
+            plt.plot([i, i], [np.mean(array[i]) - np.std(array[i]), np.mean(array[i]) + np.std(array[i])], "b-", linewidth = 0.5)
             
-    if std == True:
-        plt.plot([i*5 for i in range(rows)], [np.std(array[row]) for row in range (rows)], "b-", linewidth = 0.5)
+    if std == True: # Displays the standard deviation as another curve
+        plt.plot([i for i in range(rows)], [np.std(array[row]) for row in range (rows)], "b-", linewidth = 0.5)
     
     plt.axis([x1, x2, y1, y2])
     plt.show()
     
 
-show_plot(values,x, y, intervals=False)
+show_plot(values,x, y)
